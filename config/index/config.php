9757d0ef7016e7e4cd2301b7cd267c70b4c6a95d
siriusSupreme <guoplc@163.com>
2017-04-23 16:06:22 +0800

    ''

--- config/index/config.php
+++ config/index/config.php
@@ -5,3 +5,7 @@
  * Date: 2017/3/29
  * Time: 16:49
  */
+
+
+/*a:app c:config  m:module  f:file  d:directory*/
+/*cmf_config->cmf_database->cmd_extra->cmf_app_status->cmf_tags->amf_common->amf_lang->app_path_func->conf_path_route*/
---
+++ public/site_data/config/config.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/4/23
+ * Time: 15:13
+ */
+
+return [
+  'app_debug'=>true,
+  'app_trace'=>true,
+  'app_status'=>'debug',
+  'lang_switch_on'=>true,
+  'pathinfo_depr'=>'/',
+  'url_html_suffix'=>'html'
+];
