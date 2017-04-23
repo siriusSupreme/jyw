22fe3764c2d43c785f9c25bfd40ec1ee5bd179ab
siriusSupreme <guoplc@163.com>
2017-04-23 10:39:40 +0800

    ''

--- public/template/admin/vue/public/static/bower_components/vue-validator/test/helpers/entry.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue'
-import assert from 'power-assert'
-import plugin from '../../src/index'
-import 'babel-polyfill' // promise and etc ...
-
-Vue.use(plugin)
-
-window.Vue = Vue
-window.assert = assert
