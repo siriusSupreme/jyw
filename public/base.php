634099cf1b02bab6bd38cdbb25ca4ef19481f12a
siriusSupreme <guoplc@163.com>
2017-04-23 14:25:19 +0800

    ''

--- public/base.php
+++ public/base.php
@@ -7,7 +7,7 @@
  */
 
 /*网站根路径*/
-defined( 'ROOT_PATH') or define( 'ROOT_PATH', dirname( APP_PATH).DIRECTORY_SEPARATOR);
+defined( 'ROOT_PATH') or define( 'ROOT_PATH', dirname( realpath( APP_PATH)).DIRECTORY_SEPARATOR);
 
 /*公共资源根路径*/
 defined( 'PUBLIC_PATH') or define( 'PUBLIC_PATH', ROOT_PATH.'public'.DIRECTORY_SEPARATOR);
@@ -27,11 +27,12 @@ defined( 'CONFIG_PATH') or define( 'CONFIG_PATH', SITE_DATA_PATH.'config'.DIRECT
 defined( 'CONF_PATH') or define( 'CONF_PATH', ROOT_PATH.'config'.DIRECTORY_SEPARATOR);
 /*锁目录*/
 defined( 'LOCK_PATH') or define( 'LOCK_PATH', SITE_DATA_PATH.'locks'.DIRECTORY_SEPARATOR);
+/*配置参数目录*/
+defined( 'PARAM_PATH' ) or define( 'PARAM_PATH', SITE_DATA_PATH . 'param' . DIRECTORY_SEPARATOR );
 /*运行时目录*/
 defined( 'RUNTIME_PATH') or define( 'RUNTIME_PATH', SITE_DATA_PATH.'runtime'.DIRECTORY_SEPARATOR);
 defined( 'LOG_PATH') or define( 'LOG_PATH', RUNTIME_PATH.'log'.DIRECTORY_SEPARATOR);
 defined( 'CACHE_PATH') or define( 'CACHE_PATH', RUNTIME_PATH.'cache'.DIRECTORY_SEPARATOR);
 defined( 'TEMP_PATH') or define( 'TEMP_PATH', RUNTIME_PATH.'temp'.DIRECTORY_SEPARATOR);
-defined( 'PARAM_PATH') or define( 'PARAM_PATH', RUNTIME_PATH.'param'.DIRECTORY_SEPARATOR);
 defined( 'HTML_PATH') or define( 'HTML_PATH', RUNTIME_PATH.'html'.DIRECTORY_SEPARATOR);
 
