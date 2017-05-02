70f6a2581ea9c8eec0163a5dbaf7e47e86d66068
siriusSupreme <guoplc@163.com>
2017-05-02 14:29:39 +0800

    ''

---
+++ application/api/controller/ApiBaseController.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/5/2
+ * Time: 14:26
+ */
+
+namespace app\api\controller;
+
+
+class ApiBaseController {
+
+}
---
+++ application/api/controller/IndexController.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/5/2
+ * Time: 11:44
+ */
+
+namespace app\api\controller;
+
+
+use app\common\controller\ApiBaseController;
+
+class IndexController extends ApiBaseController {
+
+}
