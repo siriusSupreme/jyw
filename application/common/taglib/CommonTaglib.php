03696ef69269751a4b7a70521950413cfbd8e9ad
siriusSupreme <guoplc@163.com>
2017-05-09 17:59:02 +0800

    ''

---
+++ application/common/taglib/CommonTaglib.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/5/9
+ * Time: 17:58
+ */
+
+namespace app\common\taglib;
+
+
+use think\template\TagLib;
+
+class CommonTaglib extends TagLib {
+
+}
